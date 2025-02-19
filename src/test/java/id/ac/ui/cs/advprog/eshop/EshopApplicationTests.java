0cb716328b8c7845ed32d6bb86b4fb301ff9967c
Ilham Satya Nusabhakti <ilhamsn2021@gmail.com>
2025-02-19 23:13:19 +0700

    fixed file location for apptest

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -2,12 +2,25 @@ package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class EshopApplicationTests {
+class EshopApplicationTest {
+
+	@Autowired
+	private ApplicationContext context;
 
 	@Test
 	void contextLoads() {
+		// Memastikan bahwa konteks Spring Boot berhasil dimuat
+		assertThat(context).isNotNull();
 	}
 
+	@Test
+	void mainMethodRunsApplication() {
+		// Memastikan bahwa metode main dapat menjalankan aplikasi tanpa exception
+		EshopApplication.main(new String[] {});
+	}
 }
