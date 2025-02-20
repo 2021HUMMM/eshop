fa32635f9874850ab5fb3fe4df7199d3e7a66836
Ilham Satya Nusabhakti <ilhamsn2021@gmail.com>
2025-02-20 12:48:58 +0700

    changed test 'testDeleteProduct_Positive()' to 'testDeleteProductPositive()' at repository test

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -267,7 +267,7 @@ class ProductRepositoryTest {
 
     // Positive test for deleting product
     @Test
-    void testDeleteProduct_Positive() {
+    void testDeleteProductPositive() {
         Product product = new Product();
         product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
         product.setProductName("Sampo Cap Bambang");
